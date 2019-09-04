2432009fd816d2cfe39aa61d400cf4d8c2e6fa7f
alexandrew <andrew.alexandrow97@gmail.com>
2019-09-04 16:05:11 +0300

    Counter

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+// 	getInitialStase() {
+// 		return {
+// 			value: 0
+// 		}
+// 	},
+
+//   return (
+//     <h1>Hello</h1>
+//   );
+// }
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: 0
+		}
+
+		this.handleIncrement = this.handleIncrement.bind(this);
+		this.handleDecrement = this.handleDecrement.bind(this);
+	}
+
+	handleIncrement() {
+		this.setState({ value: this.state.value + 1 })
+	}
+
+	handleDecrement() {
+		this.setState({ value: this.state.value - 1 })
+	}
+
+	render() {
+		return(
+			<div>
+				<button onClick={this.handleIncrement}>+</button>
+				<h1>{this.state.value}</h1>
+				<button onClick={this.handleDecrement}>-</button>
+			</div>
+		)
+	}
 }
 
 export default App;
